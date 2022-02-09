9a195263496833f6a79ea077cc4cba5cc66d3a9d
Nicolas Weber <nicopaul.weber@gmail.com>
2022-02-09 22:34:18 +0100

    refactor in blockchain module

---
+++ blockchain/block.go
@@ -0,0 +1,45 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+// Block structur is the basic component of the blockchian
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+// DeriveHash Methode create a hash based on data and previous hash
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+// CreateBlock create and return adress to a block
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *Blockchain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *Blockchain {
+	return &Blockchain{[]*Block{Genesis()}}
+}
